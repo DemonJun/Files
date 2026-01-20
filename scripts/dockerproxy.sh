530b41e93db67e717b653d4db8315946691be186
范俊翔 <xm1107536827@gmail.com>
2026-01-20 09:07:07 +0800

    Update Docker proxy hosts and fix indentation

--- scripts/dockerproxy.sh
+++ scripts/dockerproxy.sh
@@ -2,30 +2,30 @@
 set -e
 
 image_name=$1
-proxy_hosts=("docker.1ms.run" "dockerpull.cn" "docker.1panel.live" "m.daocloud.io" "hubp.8bq.ovh")
+proxy_hosts=("docker.1ms.run" "docker.1panel.live" "docker.ketches.cn" "m.daocloud.io")
 other_mirrors=("elastic.co" "gcr.io" "ghcr.io" "k8s.io" "microsoft.com" "nvcr.io" "quay.io")
 
 # 如果传入参数为空，退出
 if [ -z "$image_name" ]; then
-    echo "No image_name found"
-    exit 1
+  echo "No image_name found"
+  exit 1
 else
-    for mirror in "${other_mirrors[@]}"; do
-        if [[ "$image_name" == *"$mirror"* ]]; then
-            echo "image need pull from $mirror, use proxy m.daocloud.io"
-            proxy_hosts=("m.daocloud.io")
-            break
-        fi
-    done
+  for mirror in "${other_mirrors[@]}"; do
+    if [[ "$image_name" == *"$mirror"* ]]; then
+      echo "image need pull from $mirror, use proxy m.daocloud.io"
+      proxy_hosts=("m.daocloud.io")
+      break
+    fi
+  done
 fi
 
 for proxy_host in "${proxy_hosts[@]}"; do
-    echo "docker pull $proxy_host/$image_name"
+  echo "docker pull $proxy_host/$image_name"
 
-    docker pull $proxy_host/$image_name
-    if [ $? -eq 0 ]; then
-        break
-    fi
+  docker pull $proxy_host/$image_name
+  if [ $? -eq 0 ]; then
+    break
+  fi
 done
 
 set -x
